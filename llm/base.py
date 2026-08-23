cb954321c04a5b2c5140dc967aaefb621ad62ac2
Ivan-Dambulov <dambulov@gmail.com>
2026-08-24 00:13:28 +0300

    Add files via upload

--- llm/base.py
+++ llm/base.py
@@ -1,18 +1,10 @@
 from abc import ABC, abstractmethod
 
-
 class BaseLLM(ABC):
-
     @abstractmethod
     def generate(self, messages):
-        """
-        Generate a complete response.
-        """
         raise NotImplementedError
 
     @abstractmethod
     def stream(self, messages):
-        """
-        Generate a response token-by-token.
-        """
         raise NotImplementedError
